b1840dac51cff12a57af56dada1f17dd0bee425c
Arthur Black <hellomynameisarthurblack@gmail.com>
2015-05-17 15:58:54 -0700

    Post message on interval

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -1,5 +1,21 @@
-module.exports = function(robot) {
-  robot.hear(/reminder/i, function(msg) {
-    msg.send("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS");
-  });
-}
+module.exports = function (robot) {
+  var config = {
+    room: '#_macbot-test',
+    msg: "Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
+    interval: 1000 * 2
+  };
+
+  var envelope = {
+    user: {
+      type: config.type || 'groupchat'
+    }
+  };
+
+  if (config.room) {
+    envelope.user.room = envelope.room = config.room;
+  }
+
+  setInterval(function () {
+    robot.send(envelope, config.msg);
+  }, config.interval);
+};
