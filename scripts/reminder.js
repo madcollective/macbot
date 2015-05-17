65269f0786342698236f7393890a101d36aa87ba
Arthur Black <hellomynameisarthurblack@gmail.com>
2015-05-17 16:40:04 -0700

    Fix channel ID
    Change channel ID to match slack format.

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -1,6 +1,6 @@
 module.exports = function (robot) {
   var config = {
-    room: '_macbot-test',
+    room: 'C04T6B8GN',
     msg: "Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
     interval: 1000 * 2
   };
