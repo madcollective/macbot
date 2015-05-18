8417071cbae8a16593d765d488f156d3081e3756
Arthur Black <hellomynameisarthurblack@gmail.com>
2015-05-18 13:51:31 -0700

    Test shout in channel
    Try different syntax <!msg> for shout in channel

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -5,9 +5,9 @@ module.exports = function (robot) {
     // room: '#_process',
     room: '#_macbot',
     // msg: "!channel Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
-    msg: '!channel testing shout',
+    msg: '<!channel> testing shout',
     // cronTime: '45 9 * * 1'
-    cronTime: "* * * * *"
+    cronTime: '* * * * *'
 
   };
 
