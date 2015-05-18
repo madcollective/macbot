1408ab7c32cdce3899f607cc45c69b05afcfa278
Christian Bundy <me@christianbundy.com>
2015-05-17 17:14:27 -0700

    Use different channel name

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -1,6 +1,6 @@
 module.exports = function (robot) {
   var config = {
-    room: '#_macbot-test',
+    room: 'macbottest',
     msg: "Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
     interval: 1000 * 2
   };
