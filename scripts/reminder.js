593f309767dda6e01e8f40034078f94d4052a33a
Christian Bundy <me@christianbundy.com>
2015-05-17 17:21:03 -0700

    Use channel name instead of ID

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -1,24 +1,11 @@
 module.exports = function (robot) {
   var config = {
-    room: 'C04T6B8GN',
+    room: '#_macbot-test',
     msg: "Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
     interval: 1000 * 2
   };
 
-  robot.messageRoom(config.room, config.msg);
-/*
-  var envelope = {
-    room: config.room,
-    user: {
-      room: config.room
-      type: config.type || 'groupchat'
-    }
-  };
-
   setInterval(function () {
-    if (robot && robot.send) {
-      robot.send(envelope, config.msg);
-    }
+    robot.messageRoom(config.room, config.msg);
   }, config.interval);
- */
 };
