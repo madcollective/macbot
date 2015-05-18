d605520d654ec56880fd1642c0cc7817ebbec4b1
Christian Bundy <me@christianbundy.com>
2015-05-17 17:18:33 -0700

    KISS

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -1,23 +1,24 @@
 module.exports = function (robot) {
   var config = {
-    room: 'macbottest',
+    room: 'C04T6B8GN',
     msg: "Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
     interval: 1000 * 2
   };
 
+  robot.messageRoom(config.room, config.msg);
+/*
   var envelope = {
+    room: config.room,
     user: {
+      room: config.room
       type: config.type || 'groupchat'
     }
   };
-
-  if (config.room) {
-    envelope.user.room = envelope.room = config.room;
-  }
 
   setInterval(function () {
     if (robot && robot.send) {
       robot.send(envelope, config.msg);
     }
   }, config.interval);
+ */
 };
