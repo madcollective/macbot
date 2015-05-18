f84edd2efb5d923c8add1880e667e9b081600bd3
Arthur Black <hellomynameisarthurblack@gmail.com>
2015-05-18 13:43:36 -0700

    testing shout in channel
    Again, testing shout functionality in channel

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -2,11 +2,13 @@ var CronJob = require('cron').CronJob;
 
 module.exports = function (robot) {
   var config = {
-    room: '#_macbot',
+    // room: '#_process',
+    room: '#_macbot'
     // msg: "!channel Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
-    msg: '!channel test'
+    msg: '!channel testing shout'
     // cronTime: '45 9 * * 1'
-    interval: 1000 * 60
+    cronTime: "* * * * *"
+
   };
 
   new CronJob(config.cronTime, function() {
