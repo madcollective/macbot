50df484a6ce079f9397fb8bc9152e2acab0cfa22
Arthur Black <hellomynameisarthurblack@gmail.com>
2015-05-18 13:28:36 -0700

    switch to test channel
    change channel and msg to test shout functionality and not bother people doing real work

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -2,9 +2,11 @@ var CronJob = require('cron').CronJob;
 
 module.exports = function (robot) {
   var config = {
-    room: '#_process',
-    msg: "!channel Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
-    cronTime: '45 9 * * 1'
+    room: '#_macbot',
+    // msg: "!channel Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
+    msg: '!channel test'
+    // cronTime: '45 9 * * 1'
+    interval: 1000 * 60
   };
 
   new CronJob(config.cronTime, function() {
