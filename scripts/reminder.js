f89cd3c06467d72ce1507870510ffc5d3aa7f038
Arthur Black <hellomynameisarthurblack@gmail.com>
2015-05-18 13:47:58 -0700

    Fix typoes
    Fix syntax errors, commas at end of line

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -3,9 +3,9 @@ var CronJob = require('cron').CronJob;
 module.exports = function (robot) {
   var config = {
     // room: '#_process',
-    room: '#_macbot'
+    room: '#_macbot',
     // msg: "!channel Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
-    msg: '!channel testing shout'
+    msg: '!channel testing shout',
     // cronTime: '45 9 * * 1'
     cronTime: "* * * * *"
 
