67e189ca40681527f60f683ac9d440681a1c6fe9
Christian Bundy <me@christianbundy.com>
2015-05-17 17:27:46 -0700

    Add cronjob

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -1,11 +1,14 @@
+var CronJob = require('cron').CronJob;
+
 module.exports = function (robot) {
   var config = {
     room: '#_macbot-test',
     msg: "Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
-    interval: 1000 * 2
   };
 
-  setInterval(function () {
+  // 45 9 * * 1
+  new CronJob('* * * * *', function() {
     robot.messageRoom(config.room, config.msg);
-  }, config.interval);
+  }, null, true, 'America/Los_Angeles');
+
 };
