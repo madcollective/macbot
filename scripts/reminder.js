890869418f3b4a779dbc6495ea32cf76dde15186
Arthur Black <hellomynameisarthurblack@gmail.com>
2015-05-26 14:57:08 -0700

    Change channel
    Change channel to more _abovethefold

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -2,7 +2,7 @@ var CronJob = require('cron').CronJob;
 
 module.exports = function (robot) {
   var config = {
-    room: '#_process',
+    room: '#_abovethefold',
     msg: "@channel Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
     cronTime: '45 9 * * 1'
   };
