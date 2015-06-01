f3b397c5c026eb663ed480711e1e8d9701de498a
Arthur Black <hellomynameisarthurblack@gmail.com>
2015-06-01 09:57:02 -0700

    Change message
    Change process to weekly in announcement message.

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -1,12 +1,14 @@
-// Description:
-//   Posts the weekly standup link every Monday at 10am.
-
 var CronJob = require('cron').CronJob;
 
 module.exports = function (robot) {
   var config = {
+<<<<<<< HEAD
     room: '#_abovethefold',
+    msg: "@channel Hey ya'll, here's the link for the weekly meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
+=======
+    room: '#_process',
     msg: "<!channel> Hey ya'll, here's the link for the process meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
+>>>>>>> e32dc39dba958066cedf99aa3b77036b51b2f593
     cronTime: '45 9 * * 1'
   };
 
