043dd90e1b4bad0e02ed5e7ce84b2ed8a1f187c3
Arthur Black <hellomynameisarthurblack@gmail.com>
2015-06-01 10:21:56 -0700

    Fix syntax
    Fix more leftover junk, syntax error for shout in channel

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -6,7 +6,7 @@ var CronJob = require('cron').CronJob;
 module.exports = function (robot) {
   var config = {
     room: '#_abovethefold',
-    msg: "@channel Hey ya'll, here's the link for the MAC projects weekly team meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
+    msg: "<!channel> Hey ya'll, here's the link for the MAC projects weekly team meeting that starts in 15 minutes. https://hangoutsapi.talkgadget.google.com/hangouts/_/gv2qg7ppm4grwujmfbvrm2iflya",
     cronTime: '45 9 * * 1'
   };
 
